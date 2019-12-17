ee3cda122f937d53c6baa34b42691b18d95bbfc1
Yime <benites.alf@gmail.com>
2019-12-16 22:39:39 -0500

    proxy with parameters imgur

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(-1);
 
 use GuzzleHttp\Client;
 
@@ -25,9 +24,18 @@ $routeCollector->setCacheFile(__DIR__.'/../CachePages/cache.file');
 
 $app->group('/api/v1', function ($app) {
     $app->get('/{section}/{sort}/{window}/{page}', function (Request $request, Response $response, $args) {
+        $url="https://api.imgur.com/3/gallery";
+        $queryParams = $request->getQueryParams();
+        $showViral=isset($queryParams['showViral'])?$queryParams['showViral']:'false';
+        $section=isset($args['section'])?$args['section']:'hot';
+        $sort=isset($args['sort'])?$args['sort']:'viral';
+        $window=isset($args['window'])?$args['window']:'day';
+        $page=isset($args['page'])?$args['page']:'0';
+        
+        $url.=DIRECTORY_SEPARATOR.$section.DIRECTORY_SEPARATOR.$sort.DIRECTORY_SEPARATOR.$window.DIRECTORY_SEPARATOR.$page.'?showViral='.$showViral;
 
         $client = new Client();
-        $dataImgur = $client->request('GET', 'https://api.imgur.com/3/gallery/hot/viral/day/1', [
+        $dataImgur = $client->request('GET', $url, [
             'headers' => [
                 'Authorization' => 'Client-ID ' . 'b35d72c36b0c7f3',
                 'Content-Type' => 'application/x-www-form-urlencoded',
@@ -37,7 +45,10 @@ $app->group('/api/v1', function ($app) {
         $response->getBody()->write($dataImgur->getBody()->getContents());
         $status = $dataImgur->getStatusCode();
 
-        return $response->withStatus($status)->withHeader('Content-Type', 'application/json');
+        return $response->withStatus($status)->withHeader('Content-Type', 'application/json')
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
 });
 
